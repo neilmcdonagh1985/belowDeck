fecf56805e8d858f4b9499caa1c5ea6f972edaec
Neil McDonagh <neiljamesmcdonagh@gmail.com>
2020-12-29 16:34:46 +0000

    go back button from details screen added

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Style, View, Text } from 'react-native';
+import { Style, View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 
-export default function ReviewDetails() {
+export default function ReviewDetails({ navigation }) {
+
+    const pressHandler = () => {
+        navigation.goBack();
+
+    }
+
     return (
         <View style={globalStyles.container}>
             <Text>Review Details Screen</Text>
+            <Button title='back to home screen' onPress={pressHandler}/>
         </View>
     )
 }
