5fb58570acf5a6f2e221a17f8da8144122032652
Neil McDonagh <neiljamesmcdonagh@gmail.com>
2020-12-29 17:54:12 +0000

    review titles listed on home page and navigate to details added

--- screens/home.js
+++ screens/home.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 export default function Home({ navigation }) {
+    const [reviews, setReviews] = useState([
+        { title: 'Hotel Pyramida', rating: 4, body: 'nice decor and mostly clean and good haircuts', key: 1 },
+        { title: 'The Globe', rating: 4, body: 'Barman talked about movies too much', key: 2 },
+        { title: 'The Red Rooms', rating: 3, body: 'Not enough people in the Irish band were Irish', key: 3 },
+    ])
 
-    const pressHandler = () => {
+    const onPressHandler = () => {
         navigation.navigate('Details')
-
     }
 
     return (
-        <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Home Screen</Text>
-            <Button title='Go to Review Details' onPress={pressHandler} />
-
+        <View>
+           {reviews.map((review, key) => 
+           <Text>{review.title}<Button onPress={onPressHandler} title='click for details' />
+           </Text>)}
         </View>
     )
+
+    // return (
+    //     reviews.map((review, index) => {
+    //         <View style={globalStyles.container}>
+    //             <Text style={globalStyles.titleText}>{review.title}</Text>
+    //         </View>
+    //     })
+    // )
+
+
+    // return (
+    //     <View style={globalStyles.container}>
+    //         <Text style={globalStyles.titleText}>Home Screen</Text>
+
+    //     </View>
+    // )
 }
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -5,15 +5,9 @@ import { globalStyles } from '../styles/global';
 
 export default function ReviewDetails({ navigation }) {
 
-    const pressHandler = () => {
-        navigation.goBack();
-
-    }
-
     return (
         <View style={globalStyles.container}>
             <Text>Review Details Screen</Text>
-            <Button title='back to home screen' onPress={pressHandler}/>
         </View>
     )
 }
