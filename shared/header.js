6b17edd75798cb1fff57fe3c5188f831c7ad45cf
Neil McDonagh <neiljamesmcdonagh@gmail.com>
2021-01-17 13:46:25 +0000

    burger nav rendered in header

--- shared/header.js
+++ shared/header.js
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import { Style, View, Text, StyleSheet } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Header() {
+
+    const openMenu = () => {
+        // navigation.openDrawer()
+    }
+
+    
     return (
         <View style={headerStyles.bar}>
-            <Text>this is the header</Text>
-            
+            <MaterialIcons name='menu' size={28} onPress={openMenu} style={headerStyles.icon}/>
+
+            <Text>this is the header</Text>           
         </View>
     )
 }
@@ -16,6 +24,9 @@ const headerStyles = StyleSheet.create({
         height: 80,
         justifyContent: 'center',
         alignItems: 'center',     
-
-    }
+    },
+    icon: {
+        position: 'absolute',
+        left: 16
+    } 
 })
