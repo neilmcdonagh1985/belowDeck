d3e7ef099b7a55280e96fce03fc2f8734d22e355
Neil McDonagh <neiljamesmcdonagh@gmail.com>
2021-03-20 18:17:40 +0000

    button made

--- shared/header.js
+++ shared/header.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Style, View, Text, StyleSheet } from 'react-native';
+import { Style, View, Text, StyleSheet, Button } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { DrawerActions } from '@react-navigation/native';
 import { useNavigation } from '@react-navigation/native';
@@ -16,22 +16,33 @@ export default function Header({ title }) {
             {/* <MaterialIcons name='menu' size={28} style={headerStyles.icon} onPress={() => navigation.openDrawer()} /> */}
             <MaterialIcons name='menu' size={28} style={headerStyles.icon} onPress={openMenu} />
 
-            <Text>{ title }</Text>           
+            <Text>{ title }</Text>
+            <Button title="Post Review" color='#ffeb3b' style={[{ width: '60'}]} />           
         </View>
     )
 }
 
 const headerStyles = StyleSheet.create({
     bar: {
+        // backgroundColor: '#fa8072',
+        // height: 80,
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        // flexDirection: 'row',
+
         backgroundColor: '#fa8072',
         height: 80,
-        justifyContent: 'center',
-        alignItems: 'center',     
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        
+             
     },
     icon: {
         position: 'absolute',
         left: 18,
         color: '#fff'
         
-    } 
+    },
+    
 })
