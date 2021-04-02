8810c1fadf093ea2d430957857a89dd82f7f6b83
Neil McDonagh <neiljamesmcdonagh@gmail.com>
2021-04-02 17:22:45 +0100

    code clean up

--- screens/home.js
+++ screens/home.js
@@ -8,25 +8,11 @@ import JSONDATA from '../MOCK_DATA.json';
 
 export default function Home({ navigation }) {
 
-    // const reviewData = JSONDATA;
     const [reviewData, setReviewData] = useState(JSONDATA);
 
-
-    // const [reviews, setReviews] = useState([
-    //     { title: 'August 21', rating: 4, body: 'Hipsters little bit', key: 'One' },
-    //     { title: 'The Beaten Dockett', rating: 4, body: 'Loud mad fellas in corner', key: 'Two' },
-    //     { title: 'City Cafe', rating: 3, body: 'Americany looking and beer and burgers', key: 'Three' },
-    // ])
-
-    // const onPressHandler = (review) => {
-    //     console.log(review)
-    //     navigation.navigate('Details', {
-    //         rating: review.rating,
-    //         body: review.body
-    //     });
-    // }
-
     return (
+        
+        
         <View>
             <FlatList
             data={reviewData}
@@ -37,23 +23,9 @@ export default function Home({ navigation }) {
                     </Card>
                 </TouchableOpacity>}
              />
+            <Text>{console.log(reviewData + 'this is the end of theeeee data')}</Text>
         </View>
+   
     )
-
-    // return (
-    //     <View style={globalStyles.container}>
-    //         <FlatList
-    //         data={reviews}
-    //         renderItem={({ item }) =>
-    //             <TouchableOpacity onPress={() => navigation.navigate('Details', item)}>
-    //                 <Card>
-    //                 <Text style={globalStyles.titleText}>{item.title}</Text>
-    //                 </Card>
-    //             </TouchableOpacity>}
-    //     />
-
-    //     </View>
-    // )
-
-
+    
 }
