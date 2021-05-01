2b9bc89cc9eac3900dad46bf609559910c32a269
Neil McDonagh <neiljamesmcdonagh@gmail.com>
2021-05-01 14:41:29 +0100

    comments about how to inmplement text area search in home

--- screens/home.js
+++ screens/home.js
@@ -1,19 +1,26 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, TextInput } from 'react-native';
 import { globalStyles } from '../styles/global';
 import { StackRouter } from 'react-navigation';
 import { FlatList } from 'react-native-gesture-handler';
 import Card from '../shared/card';
 import JSONDATA from '../MOCK_DATA.json';
+import { Fragment } from 'react';
+import Search from 'react-native-searchbar';
 
 export default function Home({ navigation }) {
 
     const [reviewData, setReviewData] = useState(JSONDATA);
 
+    // write the searchFilter function here. See video: youtube.com/watch?v=ToEd3ss4beA
+
     return (
-        
+
+    
         
         <View>
+            {/* make a TextInput here with an onChangeText prop which takes in the input text and calls a searchFilter 
+            function written above */}
             <FlatList
             keyExtractor={(item, index) => index.toString()}
             data={reviewData}
@@ -26,6 +33,8 @@ export default function Home({ navigation }) {
              />
             <Text>{console.log(reviewData + 'this is the end of the data')}</Text>
         </View>
+
+        
    
     )
     
